19abe4a47a34493fdd989bdab8e74035416ea357
Chengrui Duan <dcr309duan@gmail.com>
2020-06-02 18:59:54 +0800

    使用最新 Android Studio 和 Kotlin 进行重构的初始项目

--- app/src/main/java/com/dcr/practicedraw1/MainActivity.kt
+++ app/src/main/java/com/dcr/practicedraw1/MainActivity.kt
@@ -12,87 +12,31 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val pageModels = arrayListOf<PageModel>()
-
-    init {
-        pageModels.add(
-            PageModel(
-                R.layout.sample_color,
-                R.string.title_draw_color,
-                R.layout.practice_color
-            )
+    private val pageModels = arrayListOf(
+        PageModel(R.layout.sample_color, R.string.title_draw_color, R.layout.practice_color),
+        PageModel(R.layout.sample_circle, R.string.title_draw_circle, R.layout.practice_circle),
+        PageModel(R.layout.sample_rect, R.string.title_draw_rect, R.layout.practice_rect),
+        PageModel(R.layout.sample_point, R.string.title_draw_point, R.layout.practice_point),
+        PageModel(R.layout.sample_oval, R.string.title_draw_oval, R.layout.practice_oval),
+        PageModel(R.layout.sample_line, R.string.title_draw_line, R.layout.practice_line),
+        PageModel(
+            R.layout.sample_round_rect,
+            R.string.title_draw_round_rect,
+            R.layout.practice_round_rect
+        ),
+        PageModel(R.layout.sample_arc, R.string.title_draw_arc, R.layout.practice_arc),
+        PageModel(R.layout.sample_path, R.string.title_draw_path, R.layout.practice_path),
+        PageModel(
+            R.layout.sample_histogram,
+            R.string.title_draw_histogram,
+            R.layout.practice_histogram
+        ),
+        PageModel(
+            R.layout.sample_pie_chart,
+            R.string.title_draw_pie_chart,
+            R.layout.practice_pie_chart
         )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_circle,
-                R.string.title_draw_circle,
-                R.layout.practice_circle
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_rect,
-                R.string.title_draw_rect,
-                R.layout.practice_rect
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_point,
-                R.string.title_draw_point,
-                R.layout.practice_point
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_oval,
-                R.string.title_draw_oval,
-                R.layout.practice_oval
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_line,
-                R.string.title_draw_line,
-                R.layout.practice_line
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_round_rect,
-                R.string.title_draw_round_rect,
-                R.layout.practice_round_rect
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_arc,
-                R.string.title_draw_arc,
-                R.layout.practice_arc
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_path,
-                R.string.title_draw_path,
-                R.layout.practice_path
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_histogram,
-                R.string.title_draw_histogram,
-                R.layout.practice_histogram
-            )
-        )
-        pageModels.add(
-            PageModel(
-                R.layout.sample_pie_chart,
-                R.string.title_draw_pie_chart,
-                R.layout.practice_pie_chart
-            )
-        )
-    }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
